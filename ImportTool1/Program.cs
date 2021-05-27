5304fcfa6c32698639d9d8afb82812a673238e7e
Patrick Walters <patrickwalters@hotmail.com>
2021-05-26 23:53:22 -0400

    continuing with tool

---
+++ ImportTool1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ImportTool1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstNamesFile = "..\\..\\..\\files\\firstnames.txt";
+            string LastNamesFile = "..\\..\\..\\files\\lastnames.txt";
+
+            string ".\IMPORT_SPECIAL_" + DateTime.Now.ToString("yyyyMMddHHmm") + ".csv";
+
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
