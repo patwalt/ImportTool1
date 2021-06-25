fcbec28b229aee7a4f1ed2e24db795235d996733
Patrick Walters <patrickwalters@hotmail.com>
2021-06-25 00:42:41 -0400

    Added units file

--- ImportTool1/Program.cs
+++ ImportTool1/Program.cs
@@ -6,14 +6,42 @@ namespace ImportTool1
     {
         static void Main(string[] args)
         {
-            string FirstNamesFile = "..\\..\\..\\files\\firstnames.txt";
-            string LastNamesFile = "..\\..\\..\\files\\lastnames.txt";
+            Units orgunits = new Units();
 
-            string ".\IMPORT_SPECIAL_" + DateTime.Now.ToString("yyyyMMddHHmm") + ".csv";
+            string source = ".\\source.xlsx";
+            string destination = ".\\IMPORT_FILE_" + DateTime.Now.ToString("yyyyMMddHHmm") + ".xlsx";
 
-            
-            
-            Console.WriteLine("Hello World!");
+            string[] units = orgunits.OrgUnits();
+
+            Console.WriteLine("Welcome to the file importing tool.  This will allow you to create");
+            Console.WriteLine("one, two, a dozen, or hundreds of records that can be imported from");
+            Console.WriteLine("an Excel file.\n");
+            Console.Write("How many records would you like? (Creates Excel rows, so less than 2000): ");
+            string genNumStr = Console.ReadLine();
+            int genNum = Convert.ToInt32(genNumStr);
+
+            string[] selectedUnitName = new string[genNum];
+
+            selectedUnitName = CollectionMaker(genNum, units, units.Length);
+
+        }
+
+        public static string[] CollectionMaker(int items, string[] StringList, int sizeOfCollection)
+        {
+            Random _random = new Random();
+            int selectedOrdinal = 0;
+            int itemNumber = items;
+            int collectionSize = sizeOfCollection;
+            string[] stringList = StringList;
+            string[] resultList = new string[itemNumber];
+
+            for (int count = 0; count < itemNumber; count++)
+            {
+                selectedOrdinal = _random.Next(0, collectionSize);
+                resultList[count] = stringList[selectedOrdinal];
+            }
+
+            return resultList;
         }
     }
 }
---
+++ ImportTool1/Units.cs
@@ -0,0 +1,24 @@
+﻿namespace ImportTool1
+{
+    class Units
+    {
+        public string[] OrgUnits()
+        {
+            string orgunits = "101st Airborne, 82nd Airborne, Texian Volunteers, " +
+                              "Regimental Special Troops Battalion, Regimental Military Intelligence Battalion, " +
+                              "1st Ranger Battalion, 2nd Ranger Battalion, 3rd Ranger Battalion, " +
+                              "Air Defense Battalion, Tank Battalion, Infantry Battalion, " +
+                              "1st Recon/5th Marines, 1st Recon/1st Marines, Alpha Company/2nd Recon, " +
+                              "USS Abraham Lincoln, USS Mobile Bay, USS Lake Champlain, USS Dewey" +
+                              "USS Spruance, USS Decatur, USS Stockdale, Carrier Fighter Squadron 14 (VFA-14), " +
+                              "Strike Fighter Squadron 41 (VFA-41), Marine Fighter Attack Squadron 314 (VMFA-314), " +
+                              "Electronic Attack Squadron 133 (VAQ-133), Helicopter Sea Combat Squadron (HSC-14), " +
+                              "41st Electronic Combat Squadron, 42d Electronic Combat Squadron, " +
+                              "388th Electronic Combat Squadron, 422d Test and Evaluation Squadron, " +
+                              "357th Fighter Squadron, 355th Fighter Squadron, 94th Fighter Squadron";
+
+            string[] orgUnits = orgunits.Split(',');
+            return orgUnits;
+        }
+    }
+}
